6fe7666809185b673542599cf68fe9eb9a4cc243
Stolarz <tomek.solik@gmail.com>
2015-05-20 22:17:58 +0200

    Naprawiono wystem wybierania kolorow.

--- main.js
+++ main.js
@@ -1,34 +1,32 @@
 function changeColor(pasek) {
-  var abc = document.getElementById("pasek1");
-if(pasek.style.backgroundImage == "http://zsp2kalisz.cba.pl/wp-content/uploads/2013/09/grafika091.jpg") {
+  console.log(pasek.style.backgroundColor);
+if(pasek.style.backgroundColor =="black") {
   pasek.style.backgroundColor="#880000";
 }
-
-
-// else if (pasek.style.backgroundColor=="#880000")
-// pasek.style.backgroundColor="red";
-// else if(pasek.style.backgroundColor=="red")
-// pasek.style.backgroundColor="orange";
-// else if(pasek.style.backgroundColor=="orange")
-// pasek.style.backgroundColor="yellow";
-// else if(pasek.style.backgroundColor=="yellow")
-// pasek.style.backgroundColor="green";
-// else if(pasek.style.backgroundColor=="green")
-// pasek.style.backgroundColor="blue";
-// else if(pasek.style.backgroundColor=="blue")
-// pasek.style.backgroundColor="pink";
-// else if(pasek.style.backgroundColor=="pink" && (pasek.id=="pasek1" || pasek.id=="pasek2" || pasek.id=="pasek3" || pasek.id=="pasek5"))
-// pasek.style.backgroundColor="grey";
-// else if(pasek.style.backgroundColor=="grey" && pasek.id!="pasek5")
-// pasek.style.backgroundColor="white";
-// else if(pasek.style.backgroundColor=="grey" && pasek.id=="pasek5")
-// pasek.style.backgroundColor=="silver";
-// else if(pasek.style.backgroundColor=="pink" && pasek.id=="pasek4")
-// pasek.style.backgroundColor="silver";
-// else if(pasek.style.backgroundColor=="white")
-// pasek.style.backgroundColor="black";
-// else if(pasek.style.backgroundColor=="silver")
-// pasek.style.backgroundColor="gold";
-// else if(pasek.style.backgroundColor=="gold")
-// pasek.style.backgroundColor="black";
+else if (pasek.style.backgroundColor=="rgb(136, 0, 0)")
+pasek.style.backgroundColor="red";
+else if(pasek.style.backgroundColor=="red")
+pasek.style.backgroundColor="orange";
+else if(pasek.style.backgroundColor=="orange")
+pasek.style.backgroundColor="yellow";
+else if(pasek.style.backgroundColor=="yellow")
+pasek.style.backgroundColor="green";
+else if(pasek.style.backgroundColor=="green")
+pasek.style.backgroundColor="blue";
+else if(pasek.style.backgroundColor=="blue")
+pasek.style.backgroundColor="pink";
+else if(pasek.style.backgroundColor=="pink" && (pasek.id=="pasek1" || pasek.id=="pasek2" || pasek.id=="pasek3" || pasek.id=="pasek5"))
+pasek.style.backgroundColor="grey";
+else if(pasek.style.backgroundColor=="grey" && pasek.id!="pasek5")
+pasek.style.backgroundColor="white";
+else if(pasek.style.backgroundColor=="grey" && pasek.id=="pasek5")
+pasek.style.backgroundColor=="silver";
+else if(pasek.style.backgroundColor=="pink" && pasek.id=="pasek4")
+pasek.style.backgroundColor="silver";
+else if(pasek.style.backgroundColor=="white")
+pasek.style.backgroundColor="black";
+else if(pasek.style.backgroundColor=="silver")
+pasek.style.backgroundColor="gold";
+else if(pasek.style.backgroundColor=="gold")
+pasek.style.backgroundColor="black";
 }
